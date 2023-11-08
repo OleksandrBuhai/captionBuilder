08cd3c9fa7230b9db40c5e135155aeb0cf4af002
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2023-11-07 18:42:37 -0700

    2(UploadForm)

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import DemoSection from "@/components/DemoSection";
 import PagesHeader from "@/components/PagesHeader";
-import UploadIcon from "@/components/UploadIcon";
+import UploadForm from "@/components/UploadForm";
 
 
 
@@ -8,15 +8,11 @@ import UploadIcon from "@/components/UploadIcon";
 export default function Home() {
   return (
     <>
-    <section className='flex flex-col items-center'>
-      <PagesHeader header='Add caption to your video' subHeader='Just upload video and we make all magic '/>
-    <div className='mt-10'>
-        <button className='bg-[#f64c72]  rounded-xl  text-white s flex flex-row gap-2  max-w-xs mx-auto p-4 '>
-         <UploadIcon/>
-          Chose file</button>
-          </div>
-          </section>
-      <DemoSection/>
+      <section className='flex flex-col items-center'>
+        <PagesHeader header='Add caption to your video' subHeader='Just upload video and we make all magic ' />
+        <UploadForm />
+      </section>
+      <DemoSection />
 
     </>
   )
---
+++ src/components/UploadForm.js
@@ -0,0 +1,48 @@
+'use client'
+import { useState } from "react";
+import UploadIcon from "./UploadIcon"
+import axios from 'axios'
+
+export default function UploadForm() {
+
+    const [isUploading, setIsUploading] = useState(false)
+
+    async function upload(e) {
+        e.preventDefault();
+        console.log(e)
+        const files = e.target.files;
+
+        if (files.length > 0) {
+            const file = files[0];
+
+            try {
+                setIsUploading(true)
+                const res = await axios.postForm('/api/upload', { file, });
+                console.log(res.data);
+            } catch (error) {
+                console.error('Error during file upload:', error);
+            }
+        }
+        setIsUploading(false)
+    }
+
+    return (
+        <>
+            {isUploading && (
+                <div className="bg-black/90 text-white fixed inset-0 flex items-center">
+                    <div className="w-full text-center">
+                        <h2 className="text-4xl mb-4">Uploading</h2>
+                        <h3 className="text-xl">Please wait...</h3>
+                    </div>
+                </div>
+            )}
+            <label className='mt-10'>
+                <span className='bg-[#f64c72]  rounded-xl  text-white s flex flex-row gap-2  max-w-xs mx-auto p-4 cursor-pointer '>
+                    <UploadIcon />
+                    Chose file</span>
+                <input onChange={upload} type="file" className="hidden" />
+            </label>
+        </>
+    )
+}
+
